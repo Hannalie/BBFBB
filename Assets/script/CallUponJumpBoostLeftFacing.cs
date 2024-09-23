912091c9117c494887804b9661f29a48ea750919
hannalie <felicia.sooderqvist@gmail.com>
2024-09-23 09:58:57 +0200

    hannalie weekend additions
    musik, början på level 1, början på vatten mekanik, respawn = reload level, etc

---
+++ Assets/script/CallUponJumpBoostLeftFacing.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallUponJumpBoostLeftFacing : MonoBehaviour
+{
+    // Script is only used for "TryGetComponent" in PlayerMovement to recognize the gameObject this script is connected to.
+}
---
+++ Assets/script/KillZone.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class KillZone : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPosition;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //below script not needed
+            //other.transform.position = spawnPosition.position;
+            //other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            
+        }
+    }
+
+}
