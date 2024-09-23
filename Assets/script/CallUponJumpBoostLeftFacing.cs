1644f1b0e9b8c422ee7414c50174ba359156d785
LordoftheShirt <simon@polarcap.org>
2024-09-23 10:32:48 +0200

    PUSHING
    PLEASE WORK. PLEASE

--- Assets/script/CallUponJumpBoostLeftFacing.cs
+++ Assets/script/CallUponJumpBoostLeftFacing.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class CallUponJumpBoostLeftFacing : MonoBehaviour
 {
+    [SerializeField] public float jumpBoostLeft;
     // Script is only used for "TryGetComponent" in PlayerMovement to recognize the gameObject this script is connected to.
 }
--- Assets/script/CameraFollow.cs
+++ Assets/script/CameraFollow.cs
@@ -7,9 +7,13 @@ public class CameraFollow: MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset = new Vector3(0, 0, -10f);
     [SerializeField] private float smoothing = 1.0f;
+    [SerializeField] static public bool cameraLock = false;
     void LateUpdate()
     {
-        Vector3 newposition = Vector3.Lerp(transform.position, target.position + offset, smoothing * Time.deltaTime);
-        transform.position = newposition;
+        if (cameraLock == false)
+        {
+            Vector3 newposition = Vector3.Lerp(transform.position, target.position + offset, smoothing * Time.deltaTime);
+            transform.position = newposition;
+        }
     }
 }
